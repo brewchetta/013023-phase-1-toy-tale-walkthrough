f55bb59e84121dd879879f3bafdaef1f11a0667f
brewchetta <chett.tiller@gmail.com>
2023-02-06 15:08:14 -0500

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// ADD TOY DROPDOWN //
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +14,39 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// OUR CODE //
+
+const form = document.querySelector("form")
+// get toy collection div that we want to append
+const toyCollection = document.querySelector('#toy-collection')
+
+fetch('http://localhost:3000/toys')
+  .then( response => response.json() )
+  .then( data => {
+    console.log(data)
+    data.forEach( toy => {
+      // create a new div that holds the toy info
+      const toyDiv = document.createElement( 'div' )
+      toyDiv.className = "card"
+      // add information from json file to the div
+      // create the elements we'll need
+      const toyName = document.createElement( 'h2' )
+      const toyImage = document.createElement( 'img' )
+      const toyLikes = document.createElement( 'p' )
+      const toyButton = document.createElement( 'button' )
+      // set attributes for variables
+      // toyName.setAttribute('textContent', "sup")
+      toyName.textContent = toy.name
+      toyImage.src = toy.image
+      toyImage.className = "toy-avatar"
+      toyLikes.textContent = `${toy.likes} Likes`
+      toyButton.className = "like-btn"
+      toyButton.id = toy.id
+      // append the things
+      toyDiv.append(toyName, toyImage, toyLikes, toyButton)
+      toyCollection.appendChild(toyDiv)
+    } )
+  } )
+
+  // make a div
